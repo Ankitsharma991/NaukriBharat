ccc899a518bb0d9f5020bb386f15350325b09937
Aadarsh <aadarsh9811594152@gmail.com>
2023-04-26 18:54:50 +0530

    code homescreen and user code refactored and added comments

--- components/pages/screen/homescreen/homescreen.js
+++ components/pages/screen/homescreen/homescreen.js
@@ -8,61 +8,103 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
-import SelectDropdown from 'react-native-select-dropdown'
+import SelectDropdown from "react-native-select-dropdown";
 import User from "../profile/user/user";
+import { Alert } from "react-native";
 
 export default function HomeScreen() {
-  const profession = ["actor", "singer", "dancer", "model", "musician", "comedian", "artist", "writer", "director", "producer", "designer", "photographer", "editor", "barber","police","plumber","doctor","teacher","engineer","lawyer","accountant","architect","farmer","chef","pilot","nurse","firefighter","scientist","astronaut","judge","policeman",
-  "labour"];
+  const professions = [
+    "actor",
+    "singer",
+    "dancer",
+    "model",
+    "musician",
+    "comedian",
+    "artist",
+    "writer",
+    "director",
+    "producer",
+    "designer",
+    "photographer",
+    "editor",
+    "barber",
+    "police",
+    "plumber",
+    "doctor",
+    "teacher",
+    "engineer",
+    "lawyer",
+    "accountant",
+    "architect",
+    "farmer",
+    "chef",
+    "pilot",
+    "nurse",
+    "firefighter",
+    "scientist",
+    "astronaut",
+    "judge",
+    "policeman",
+    "labour",
+  ];
+
+  const handleMenuButtonClick = () => {
+    Alert.alert("Menu button clicked");
+  };
+
+  const handleProfileButtonClick = () => {
+    Alert.alert("Profile button clicked");
+  };
+
+  const handlePostButtonClick = () => {
+    Alert.alert("Post button clicked");
+  };
+
   return (
-    
     <View style={styles.container}>
-      
       {/* Menu icon on the upper left */}
-      <TouchableOpacity style={styles.menuButton}>
-        <Ionicons name="menu-outline" size={30} color="black" />
+      <TouchableOpacity style={styles.menuButton} onPress={handleMenuButtonClick}>
+        <Ionicons name="menu-outline" size={40} color="black" />
       </TouchableOpacity>
 
       {/* Profile icon on the upper right */}
-      <TouchableOpacity style={styles.profileButton}>
-        <Ionicons name="person-circle-outline" size={30} color="black" />
+      <TouchableOpacity style={styles.profileButton} onPress={handleProfileButtonClick}>
+        <Ionicons name="person-circle-outline" size={40} color="black" />
       </TouchableOpacity>
 
-      <User/>
+      <View style={styles.fullScreen}>
+        <User />
+      </View>
 
-      {/* Body of the home screen */} 
+      {/* Body of the home screen */}
       <View style={styles.professionFilter}>
-
-  
-
         <SelectDropdown
-          data={profession}
+          data={professions}
           onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index)
+            console.log(selectedItem, index);
           }}
           buttonTextAfterSelection={(selectedItem, index) => {
             // text represented after item is selected
             // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem
+            return selectedItem;
           }}
           rowTextForSelection={(item, index) => {
             // text represented for each item in dropdown
             // if data array is an array of objects then return item.property to represent item in dropdown
-            return item
+            return item;
           }}
         />
-            
       </View>
       <View style={styles.postRequirement}>
         <TextInput placeholder="Post a requirement" style={styles.inputField} />
-        <TouchableOpacity style={styles.postButton}>
-              <Text style={styles.postText}>Post</Text>
-            </TouchableOpacity>
+        <TouchableOpacity style={styles.postButton} onPress={handlePostButtonClick}>
+          <Text style={styles.postText}>Post</Text>
+        </TouchableOpacity>
       </View>
- 
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -70,24 +112,34 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 50,
     position: "relative",
-    backgroundColor: "#fff",
+    backgroundColor: "#ffc0cb",
+
   },
   menuButton: {
     position: "absolute",
     top: 10,
-    left: 10,
-    padding: 10,
+    left: 5,
+    padding: 5,
+
   },
   profileButton: {
     position: "absolute",
     top: 10,
     right: 10,
     padding: 10,
+
+  },
+  fullScreen: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+    marginHorizontal: -40,
   },
   body: {
     flex: 2,
     alignItems: "center",
     justifyContent: "center",
+
   },
   title: {
     fontSize: 24,
@@ -104,14 +156,13 @@ const styles = StyleSheet.create({
   professionFilter: {
     right: 5,
     padding: 10,
-    marginBottom: 200,
-    
+    marginBottom: 10,
 
   },
   postRequirement: {
-    position:"absolute",
-    top:18,
-    left:50,
+    position: "absolute",
+    top: 18,
+    left: 50,
 
   },
   inputField: {
@@ -121,16 +172,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     margin: 4,
+
   },
-  postButton:{
-    position:"absolute",
+  postButton: {
+    position: "absolute",
     backgroundColor: "#00ff00",
     padding: 2,
     margin: 8,
     height: 25,
     width: 50,
     borderRadius: 10,
-    left:210
-    
-  }
+    left: 210,
+
+  },
 });
--- components/pages/screen/profile/user/user.js
+++ components/pages/screen/profile/user/user.js
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const User = () => {
+  // Define state variables for loading state and API data
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
+  // Fetch data from API using async/await
   const fetchApiData = async () => {
     try {
       const response = await fetch(
@@ -20,10 +29,12 @@ const User = () => {
     }
   };
 
+  // Call the fetchApiData function when the component mounts
   useEffect(() => {
     fetchApiData();
   }, []);
 
+  // Handle the button press events
   const handleCallPress = () => {
     alert("Call button clicked");
   };
@@ -32,30 +43,72 @@ const User = () => {
     alert("Message button clicked");
   };
 
+  const handleImagePress = () => {
+    alert("Image clicked!");
+  };
+
+  const handleTitlePress = (title) => {
+    alert(`${title} clicked`);
+  };
+
+  // Define the render function for each movie item in the list
+  const renderMovieItem = ({ item }) => (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 30,
+      }}
+    >
+      <TouchableOpacity onPress={handleImagePress}>
+        <View style={{ overflow: "hidden", borderRadius: 50 }}>
+          <Image
+            style={{ width: 80, height: 80 }}
+            source={{ uri: item.Poster }}
+          />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleTitlePress(item.Title)}>
+        <View
+          style={{
+            marginLeft: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text>{item.Title}</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleCallPress}>
+        <Icon
+          name="phone"
+          size={20}
+          color="black"
+          style={{ marginLeft: 10 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleMessagePress}>
+        <Icon
+          name="comment"
+          size={20}
+          color="black"
+          style={{ marginLeft: 10 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
+      {/* Display an activity indicator while data is being fetched */}
       {isLoading ? (
         <ActivityIndicator />
       ) : (
+        /* Render the flat list with the fetched data and the renderMovieItem function */
         <FlatList
           data={data}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <View style={{flexDirection: 'row', alignItems: 'center', marginBottom: 10}}>
-              <View style={{overflow: 'hidden', borderRadius: 50}}>
-                <Image style={{ width: 100, height: 100 }} source={{ uri: item.Poster }} />
-              </View>
-              <View style={{marginLeft: 10, flexDirection: 'row', alignItems: 'center'}}>
-                <Text>{item.Title}</Text>
-                <TouchableOpacity onPress={handleCallPress}>
-                  <Icon name="phone" size={20} color="black" style={{marginLeft: 10}} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleMessagePress}>
-                  <Icon name="comment" size={20} color="black" style={{marginLeft: 10}} />
-                </TouchableOpacity>
-              </View>
-            </View>
-          )}
+          renderItem={renderMovieItem}
         />
       )}
     </View>

46b543353be17ab54fb04b5afea3aa446ed5e9c0
Aadarsh <aadarsh9811594152@gmail.com>
2023-04-27 18:28:13 +0530

    Refactor LoginPage component to fix bugs and add comments.

--- components/pages/loginpage/loginpage.js
+++ components/pages/loginpage/loginpage.js
@@ -1,43 +1,89 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+// Importing required modules and functions
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import firebase from "../../../database/firebase";
+import React, { useState, useEffect } from "react";
+
+// Initializing Firebase authentication
+const auth = getAuth(firebase);
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // Initializing state variables for login form data and logged in user
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
-  const handleLogin = () => {
-    // handle user authentication logic here
-  }
+  // Getting navigation object for navigation between screens
+  const navigation = useNavigation();
 
+  // Function to handle login form submit
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, loginData.email, loginData.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setLoggedInUser(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
+
+  // Effect to navigate to home screen when user is logged in
+  useEffect(() => {
+    if (loggedInUser) {
+      navigation.navigate("HomeScreen");
+    }
+  }, [loggedInUser]);
+
+  // Function to handle login form input change
+  const handleLoginFormInputChange = (name, value) => {
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  // Rendering login form with input fields for email and password, login button,
+  // and a register button to navigate to the signup page
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
         placeholder="Email"
-        onChangeText={setEmail}
-        value={email}
+        onChangeText={(text) => handleLoginFormInputChange("email", text)}
+        value={loginData.email}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
-        onChangeText={setPassword}
-        value={password}
+        onChangeText={(text) => handleLoginFormInputChange("password", text)}
+        value={loginData.password}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} />
+
+      <Button title="Login" onPress={handleLoginFormSubmit} />
+      <Text style={styles.professionText}>Don't have an account?</Text>
+      <Text style={styles.professionText}>Register here:</Text>
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate("SignupPage")}
+      />
     </View>
   );
 };
 
+// Styling for the login form
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     height: 40,
-    width: '90%',
+    width: "90%",
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 10,
